30b09fd004470b4c921ed64a7dc1159c0097fedf
Mike van Egmond <mikevanegmond1998@outlook.com>
2023-05-03 13:06:11 +0200

    feat: basic setup of express api

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
